8ef303cb5a61442d5635de6a1ecc8939645a5792
kushal <collabskus@gmail.com>
2026-05-27 19:54:45 -0400

    add all files

--- docs/llm/vendor/files/MoonPhase.cs
+++
@@ -1,9 +0,0 @@
-namespace CollabsKus.BlazorWebAssembly.Models;
-
-public class MoonPhase
-{
-    public string Name { get; set; } = string.Empty;
-    public string Icon { get; set; } = string.Empty;
-    public double Illumination { get; set; }
-    public double Age { get; set; }
-}
